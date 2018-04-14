bb954c468e92c797720c4a3f744cb1ba72ebf102
Trond Einar Snekvik <trond.einar.snekvik@nordicsemi.no>
2018-04-14 18:07:34 +0200

    Add basic multithread wrappers

--- backend/include/utils.h
+++ backend/include/utils.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <stdbool.h>
 #include <stdlib.h>
-#if _WIN32
+#ifdef _WIN32
 #include <crtdbg.h>
+#include <Windows.h>
 #endif
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -26,6 +27,77 @@ typedef struct
     unsigned line_count;
 } line_array_t;
 
+typedef struct thread thread_t;
+
+typedef struct
+{
+#ifdef _WIN32
+    CRITICAL_SECTION critical_section;
+#else
+    #error "Not implemented."
+#endif
+} mutex_t;
+
+typedef struct
+{
+#ifdef _WIN32
+    HANDLE sem;
+#else
+    #error "Not implemented."
+#endif
+} semaphore_t;
+
+typedef struct
+{
+#ifdef _WIN32
+    LONG value;
+#endif
+} atomic_counter_t;
+
+typedef struct
+{
+    mutex_t mut;
+#ifdef _WIN32
+    unsigned users;
+    HANDLE owner_event;
+    HANDLE user_event;
+#endif
+} shared_resource_t;
+
+typedef enum
+{
+    THREAD_PRIO_LOW,
+    THREAD_PRIO_NORMAL,
+    THREAD_PRIO_HIGH,
+} thread_priority_t;
+
+typedef void (*thread_function_t)(void * p_args);
+
 line_array_t * line_array(const char * p_string);
 
 void line_array_free(line_array_t * p_line_array);
+
+
+thread_t * thread_start(thread_function_t function, void * p_args, thread_priority_t priority);
+
+void thread_join(thread_t * p_thread);
+void thread_cancel(thread_t * p_thread);
+
+void mutex_init(mutex_t *  p_mut);
+void mutex_take(mutex_t * p_mut);
+bool mutex_try_take(mutex_t * p_mut);
+void mutex_release(mutex_t * p_mut);
+void mutex_free(mutex_t * p_mut);
+
+void semaphore_init(semaphore_t * p_sem, unsigned max_count);
+void semaphore_wait(semaphore_t * p_sem);
+void semaphore_signal(semaphore_t * p_sem);
+
+int atomic_get_and_add(atomic_counter_t * p_counter);
+int atomic_get_and_sub(atomic_counter_t * p_counter);
+
+void shared_resource_init(shared_resource_t * p_resource);
+void shared_resource_borrow(shared_resource_t * p_resource);
+void shared_resource_release(shared_resource_t * p_resource);
+void shared_resource_lock(shared_resource_t * p_resource);
+void shared_resource_unlock(shared_resource_t * p_resource);
--- backend/src/utils.c
+++ backend/src/utils.c
@@ -5,6 +5,25 @@
 #include "log.h"
 #include "hashtable.h"
 
+#ifdef _WIN32
+    #include <Windows.h>
+    #define STACK_SIZE 4096
+#else
+    #include <pthread.h>
+#endif
+
+struct thread
+{
+    thread_function_t func;
+    void * p_args;
+#ifdef _WIN32
+    DWORD id;
+    HANDLE handle;
+#else
+    pthread_t pthread;
+#endif
+};
+
 
 line_array_t * line_array(const char * p_string)
 {
@@ -38,8 +57,6 @@ line_array_t * line_array(const char * p_string)
     return p_result;
 }
 
-
-
 void line_array_free(line_array_t * p_line_array)
 {
     if (p_line_array)
@@ -52,3 +69,135 @@ void line_array_free(line_array_t * p_line_array)
         FREE(p_line_array);
     }
 }
+
+#ifdef _WIN32
+static DWORD WINAPI win_thread(void * p_args)
+{
+    thread_t * p_thread = p_args;
+    p_thread->func(p_thread->p_args);
+    return 0;
+}
+
+thread_t * thread_start(thread_function_t function, void * p_args, thread_priority_t priority)
+{
+    thread_t * p_thread = MALLOC(sizeof(thread_t));
+    ASSERT(p_thread);
+    p_thread->func = function;
+    p_thread->p_args = p_args;
+
+    int win32_priority[] = {
+        [THREAD_PRIO_LOW]    = THREAD_PRIORITY_LOWEST,
+        [THREAD_PRIO_NORMAL] = THREAD_PRIORITY_NORMAL,
+        [THREAD_PRIO_HIGH]   = THREAD_PRIORITY_ABOVE_NORMAL,
+    };
+
+    p_thread->handle = CreateThread(NULL, STACK_SIZE, win_thread, p_thread, CREATE_SUSPENDED, &p_thread->id);
+    SetThreadPriority(p_thread->handle, win32_priority[priority]);
+    ResumeThread(p_thread->handle);
+    return p_thread;
+}
+
+void thread_join(thread_t * p_thread)
+{
+    WaitForSingleObject(p_thread->handle, INFINITE);
+}
+
+void mutex_init(mutex_t *  p_mut)
+{
+    InitializeCriticalSection(&p_mut->critical_section);
+}
+
+void mutex_take(mutex_t * p_mut)
+{
+    EnterCriticalSection(&p_mut->critical_section);
+}
+
+bool mutex_try_take(mutex_t * p_mut)
+{
+    return TryEnterCriticalSection(&p_mut->critical_section);
+}
+
+void mutex_release(mutex_t * p_mut)
+{
+    LeaveCriticalSection(&p_mut->critical_section);
+}
+
+void mutex_free(mutex_t * p_mut)
+{
+}
+
+void semaphore_init(semaphore_t * p_sem, unsigned max_count)
+{
+    p_sem->sem = CreateSemaphore(NULL, 0, max_count, NULL);
+}
+
+void semaphore_wait(semaphore_t * p_sem)
+{
+    WaitForSingleObject(p_sem->sem, 0);
+}
+
+void semaphore_signal(semaphore_t * p_sem)
+{
+    ReleaseSemaphore(p_sem->sem, 1, NULL);
+}
+
+int atomic_get_and_add(atomic_counter_t * p_flag)
+{
+    return (int) InterlockedAdd(&p_flag->value, 1);
+}
+
+int atomic_get_and_sub(atomic_counter_t * p_flag)
+{
+    return (int) InterlockedAdd(&p_flag->value, -1);
+}
+
+void shared_resource_init(shared_resource_t * p_resource)
+{
+    mutex_init(&p_resource->mut);
+    p_resource->user_event = CreateEvent(NULL, true, true, NULL);
+    p_resource->owner_event = CreateEvent(NULL, true, true, NULL);
+    p_resource->users = 0;
+}
+
+void shared_resource_borrow(shared_resource_t * p_resource)
+{
+    mutex_take(&p_resource->mut);
+    if (p_resource->users++ == 0)
+    {
+        ResetEvent(p_resource->user_event);
+    }
+    mutex_release(&p_resource->mut);
+    WaitForSingleObject(p_resource->owner_event, 0);
+}
+
+void shared_resource_release(shared_resource_t * p_resource)
+{
+    mutex_take(&p_resource->mut);
+    ASSERT(p_resource->users > 0);
+    if (--p_resource->users == 0)
+    {
+        SetEvent(p_resource->user_event);
+    }
+    mutex_release(&p_resource->mut);
+}
+
+void shared_resource_lock(shared_resource_t * p_resource)
+{
+    ResetEvent(p_resource->owner_event);
+    WaitForSingleObject(p_resource->user_event, 0);
+}
+
+void shared_resource_unlock(shared_resource_t * p_resource)
+{
+    SetEvent(p_resource->owner_event);
+}
+
+#else
+static void * posix_thread(void * p_args)
+{
+    thread_t * p_thread = p_args;
+    p_thread->func(p_thread->p_args);
+    return NULL;
+}
+#endif
+
